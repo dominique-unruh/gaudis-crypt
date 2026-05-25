dfd38b27a7606eefa08ac99ca02669e6ff1236bc
Dominique Unruh <git.qischair.51byei@rwth.unruh.de>
2026-05-25 04:24:05 +0200

    Drafts of various lens-theorems.

--- PlonkLean/Lens.lean
+++ PlonkLean/Lens.lean
@@ -1,4 +1,7 @@
 import Mathlib.CategoryTheory.Category.Basic
+import Mathlib.Data.ZMod.Basic
+import Mathlib.GroupTheory.Submonoid.Centralizer
+import PlonkLean.Misc
 
 structure Lens (a : Type u) (b : Type v) where
   get : b -> a
@@ -77,4 +80,117 @@ def Lens.bijection (e : a ≃ b) : Lens a b where
   set_set _ _ _ := rfl
   get_set s := e.apply_symm_apply s
 
+structure LensIn.{u,v} (m : Type u) : Type _ where
+  content : Type v
+  lens : Lens content m
 
+def LensIn.mk' (lens : Lens a m) : LensIn m := ⟨a, lens⟩
+
+def IsoLens (lens : Lens a b) := Function.Bijective lens.get
+
+instance : Preorder (LensIn m) where
+  le x y := exists z : Lens x.content y.content, IsoLens z ∧ chain y.lens z = x.lens
+  le_refl := sorry
+  le_trans := sorry
+
+abbrev bit := ZMod 2
+
+def example_lens_1 : Lens bit (bit × bit × bit) where
+  get := fun (x,y,z) => x + y
+  set := fun a (x,y,z) => (x, x+a, z)
+  set_get := sorry
+  get_set := sorry
+  set_set := sorry
+
+def example_lens_2 : Lens bit (bit × bit × bit) where
+  get := fun (x,y,z) => y + z
+  set := fun a (x,y,z) => (x, z+a, z)
+  set_get := sorry
+  get_set := sorry
+  set_set := sorry
+
+def upper_bound_1 : Lens (bit × bit) (bit × bit × bit) where
+  get := fun (x,y,z) => (x + y, y + z)
+  set := fun (a,b) (x,y,z) => (x,a+x,a+b+x)
+  set_get := sorry
+  get_set := sorry
+  set_set := sorry
+
+def upper_bound_2 : Lens (bit × bit) (bit × bit × bit) where
+  get := fun (x,y,z) => (x + y, y + z)
+  set := fun (a,b) (x,y,z) => (a+b+z, b+z, z)
+  set_get := sorry
+  get_set := sorry
+  set_set := sorry
+
+lemma ub11 : LensIn.mk' upper_bound_1 ≥ LensIn.mk' example_lens_1 := sorry
+lemma ub12 : LensIn.mk' upper_bound_1 ≥ LensIn.mk' example_lens_2 := sorry
+lemma ub21 : LensIn.mk' upper_bound_2 ≥ LensIn.mk' example_lens_1 := sorry
+lemma ub22 : LensIn.mk' upper_bound_2 ≥ LensIn.mk' example_lens_2 := sorry
+
+theorem no_least_lens : ¬ exists l : LensIn (bit × bit × bit),
+  IsLUB {LensIn.mk' example_lens_1, LensIn.mk' example_lens_2} l := by
+  /-
+  Proof sketch:
+  - Both upper_bound_1 and upper_bound_2 are upper bounds.
+  - upper_bound_1 and upper_bound_2 are incomparable. Reason:
+      - If  upper_bound_1 = chain upper_bound_2 x,
+        then (chain upper_bound_2 x).set cannot modify the third component of the (bit x bit x bit) memory
+        but upper_bound_1 does. So they can't be equal.
+      - Analogous in the other direction
+  - So if the LUB l exists, it must be strictly smaller than both upper_bound_1 and upper_bound_2
+  - If l is strictly smaller than upper_bound_1, then l.content must have smaller cardinality than
+    (LensIn.mk' upper_bound_1).content. And it must be a divider of that cardinality.
+  - Since that cardinality is 4, l.content must have cardinality 2.
+  - l >= example_lens_2, example_lens_2.content and l.content have same cardinality (namely 2)
+  - Consequence: l ~~ example_lens_2  (~~ meaning both <= and >=).
+  - Since l is an upper bound of example_lens_1, this implies example_lens_1 <= example_lens_2.
+  - But that's incorrect, since example_lens_1.set can change the first component of the memory, and example_lens_2.set only the second
+  - We have a contradiction, so l cannot exist
+  -/
+  sorry
+
+def Lens.update (lens : Lens a m) (f : a → a) : m → m := fun x => lens.set (f (lens.get x)) x
+
+
+structure LensRange (m : Type _) where
+  updates :
+    letI inst : Monoid (m → m) := sorry
+    Submonoid (m → m)
+  double_commutant :
+    letI inst : Monoid (m → m) := _
+    Submonoid.centralizer (Submonoid.centralizer updates.carrier).carrier = updates
+
+
+
+def Lens.range (lens : Lens a m) : LensRange m where
+  updates :=
+    letI inst : Monoid (m → m) := _
+    ⟨⟨Set.image lens.update ⊤, sorry⟩, sorry⟩
+  double_commutant := sorry
+
+instance : CompleteLattice (LensRange m) where
+  le x y := x.updates ≤ y.updates
+  bot := sorry -- only id
+  top := sorry -- everything
+  inf := sorry -- intersection
+  sup := sorry -- double commutant of union
+  sSup := sorry -- double commutant of union
+  sInf := sorry -- intersection
+  le_refl := sorry
+  le_trans := sorry
+  le_antisymm := sorry
+  le_sup_left := sorry
+  le_sup_right := sorry
+  le_inf := sorry
+  sup_le := sorry
+  inf_le_left := sorry
+  bot_le := sorry
+  inf_le_right := sorry
+  isLUB_sSup := sorry
+  isGLB_sInf := sorry
+  le_top := sorry
+
+
+theorem Lens.range_defines_preorder (x : Lens a m) (y : Lens b m) :
+  x.range ≤ y.range ↔ LensIn.mk' x ≤ LensIn.mk' y := sorry
